d93cf139b1f065a37169f579b130d0ca30a7ba52
KarenEYoval <148494152+KarenEYoval@users.noreply.github.com>
2023-12-06 11:15:51 -0600

    Update proyectofinal.sql

--- proyectofinal.sql
+++ proyectofinal.sql
@@ -1,65 +1,67 @@
 CREATE DATABASE IF NOT EXISTS base_de_datos; 
 USE base_de_datos; 
 
-CREATE TABLE IF NOT EXISTS Alumnos ( 
+CREATE TABLE IF NOT EXISTS Alumno ( 
     Matricula INT UNSIGNED PRIMARY KEY, 
-    Nombre VARCHAR(50), 
-    ApellidoPaterno VARCHAR(50), 
-    ApellidoMaterno VARCHAR(50), 
-    Email VARCHAR(50), 
-    Edad INT 
+    nombres VARCHAR(50), 
+    paterno VARCHAR(50), 
+    materno VARCHAR(50), 
+    email VARCHAR(50), 
+    edad INT 
 ); 
 
-CREATE TABLE IF NOT EXISTS Carreras ( 
+CREATE TABLE IF NOT EXISTS Cursa (
+    Matricula INT UNSIGNED,
+    NCR INT UNSIGNED,
+    Calificacion FLOAT,
+    FOREIGN KEY (Matricula) REFERENCES Alumnos(Matricula),
+    FOREIGN KEY (NCR) REFERENCES Oferta(NCR)
+);
+
+
+CREATE TABLE IF NOT EXISTS Carrera ( 
     CodigoCarrera INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-    NombreCarrera VARCHAR(100), 
-    Descripcion VARCHAR(55) 
+    nombreCarrera VARCHAR(100), 
+    descripcion VARCHAR(55) 
 ); 
 
-CREATE TABLE IF NOT EXISTS Materias ( 
-    CodigoMateria INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-    NombreMateria VARCHAR(100), 
-    Descripcion VARCHAR(55), 
-    Creditos INT, 
-    Bloque INT, 
-    CodigoCarrera INT
+CREATE TABLE IF NOT EXISTS Materia (
+    CodigoMateria INT UNSIGNED PRIMARY KEY,
+    CodigoCarrera INT UNSIGNED,
+    CodigoArea INT UNSIGNED,
+    NombreMateria VARCHAR(100),
+    Descripcion VARCHAR(55),
+    Creditos INT,
+    Bloque INT,
+    FOREIGN KEY (CodigoCarrera) REFERENCES Carreras(CodigoCarrera),
+    FOREIGN KEY (CodigoArea) REFERENCES AreasDeFormacion(CodigoArea)
+);
+
+
+
+CREATE TABLE IF NOT EXISTS Area ( 
+    CodArea INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    nombre VARCHAR(100) 
 ); 
 
-
-CREATE TABLE IF NOT EXISTS AreasDeFormacion ( 
-    CodigoArea INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-    NombreArea VARCHAR(100) 
-); 
-
-CREATE TABLE IF NOT EXISTS PeriodosEscolares ( 
-    CodigoPeriodo INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+CREATE TABLE IF NOT EXISTS Periodos ( 
+    CodPeriodo INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
     NombrePeriodo VARCHAR(50), 
     FechaInicio DATE, 
     FechaFin DATE 
 ); 
 
-CREATE TABLE IF NOT EXISTS Profesores ( 
-    NumeroPersonal INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+CREATE TABLE IF NOT EXISTS Profesor ( 
+    numeroPersonal INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
     Nombre VARCHAR(50), 
-    ApellidoPaterno VARCHAR(50), 
-    ApellidoMaterno VARCHAR(50), 
+    paterno VARCHAR(50), 
+    materno VARCHAR(50), 
     Email VARCHAR(100), 
-    GradoEstudios VARCHAR(50) 
+    licenciatura VARCHAR(50) 
+    maestria VARCHAR(50)
+    doctorado VARCHAR(50)
 ); 
 
-CREATE TABLE IF NOT EXISTS estudio (
-    MatriculaM VARCHAR(50),
-    codMateria INT UNSIGNED,
-    calificacion FLOAT
-);
-
-CREATE TABLE IF NOT EXISTS Materias_Profesores (
-    Id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    CodigoMateria INT UNSIGNED,
-    NumeroPersonal INT UNSIGNED,
-    FOREIGN KEY (CodigoMateria) REFERENCES Materias(CodigoMateria),
-    FOREIGN KEY (NumeroPersonal) REFERENCES Profesores(NumeroPersonal)
-);
 
 CREATE TABLE IF NOT EXISTS Oferta (
     NCR INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -73,7 +75,10 @@ CREATE TABLE IF NOT EXISTS Oferta (
 
 
 
-INSERT INTO Alumnos (Matricula, Nombre, ApellidoPaterno, ApellidoMaterno, Email, Edad)
+
+
+
+INSERT INTO AlumnoS (Matricula, Nombre, ApellidoPaterno, ApellidoMaterno, Email, Edad)
 VALUES 
     (123, 'Karen', 'Lopez', 'Yoval', 'karenyoval26@jamil.com', 19),
     (222, 'Harry', 'Eduard', 'Styles', 'styles@example.com', 19),
@@ -87,7 +92,7 @@ VALUES
     (101, 'Zack', 'Efron', 'Lopez', 'zack@exmple.com', 19);
 
 
-INSERT INTO Carreras (CodigoCarrera, NombreCarrera, Descripcion)
+INSERT INTO Carrera (CodigoCarrera, NombreCarrera, Descripcion)
 VALUES 
     (1, 'LIS', 'Rama de la Ingeniería que estudia el hardware'),
     (2, 'LICIC', 'Encargada de estudiar la administración de métodos'),
@@ -97,7 +102,7 @@ VALUES
 
 
 
-INSERT INTO Profesores (NumeroPersonal, Nombre, Email, GradoEstudios)
+INSERT INTO Profesor (NumeroPersonal, Nombre, Email, GradoEstudios)
 VALUES 
     (9284, 'Pedro Martínez', 'pedro@example.com', 'Doctorado'),
     (8285, 'Ana Gómez', 'ana@example.com', 'Maestría'),
@@ -105,15 +110,19 @@ VALUES
     (6187, 'Guillermo Humberto', 'guille@example.com', 'Doctorado'),
     (7288, 'Juana Elisa', 'juana@example.com', 'Licenciatura');
 
-INSERT INTO Materias (CodigoMateria, NombreMateria, Descripcion, Creditos, Bloque)
-VALUES 
-    (1112, 'Computacion basica', 'Conocimientos básicos de computación', 4, 11),
-    (2223, 'Matematicas discretas', 'Conocimientos básicos de computación', 5, 22),
-    (3334, 'Sistemas Operativos', 'Conocimientos básicos de computación', 6, 33),
-    (4445, 'Experiencia recepcional', 'conocimientos adquiridos a lo largo de la carrera', 6, 77),
-    (5556, 'Eleccion Libre', 'Actividades recreativas', 4, 2);
 
-INSERT INTO AreasDeFormacion (CodigoArea, NombreArea)
+INSERT INTO Materia (CodigoMateria, CodigoCarrera, CodigoArea, NombreMateria, Descripcion, Creditos, Bloque)
+VALUES 
+    (1, 1, 50, 'Introducción a la Informática', 'Conceptos básicos de computación', 4, 11),
+    (2, 1, 60, 'Estructuras de Datos', 'Manejo y organización de datos', 5, 22),
+    (3, 2, 70, 'Gestión Empresarial', 'Fundamentos de administración de empresas', 6, 33),
+    (4, 2, 80, 'Contabilidad Financiera', 'Registro y análisis de transacciones financieras', 6, 77),
+    (5, 3, 90, 'Historia del Arte', 'Exploración de movimientos artísticos', 4, 2);
+
+
+
+
+INSERT INTO Area (CodigoArea, NombreArea)
 VALUES 
     (50, 'Area diciplinar'),
     (60, 'Área de Formación básica'),
@@ -122,7 +131,7 @@ VALUES
     (90, 'Área de formación de elección libre');
 
 
-INSERT INTO PeriodosEscolares (CodigoPeriodo, NombrePeriodo, FechaInicio, FechaFin)
+INSERT INTO Periodo (CodigoPeriodo, NombrePeriodo, FechaInicio, FechaFin)
 VALUES 
     (0101, 'Primer', '2018-01-10', '2018-03-20'),
     (0202, 'Segundo', '2018-06-01', '2018-09-26'),
@@ -133,21 +142,15 @@ VALUES
     (0707, 'Septimo', '2019-12-24', '2020-01-10'),
     (0808, 'Octavo', '2020-03-30', '2020-06-27');
 
-INSERT INTO estudio (MatriculaM, codMateria, calificacion)
-VALUES
-    ('zS22016126', 1, 10),
-    ('zS22016236', 2, 5),
-    ('zS22633654', 3, 8),
-    ('zS22856985', 4, 9),
-    ('zS22638754', 5, 7);
+INSERT INTO Cursa (Matricula, NCR, Calificacion)
+VALUES 
+    (123, 1, 9.5),
+    (222, 2, 8),
+    (333, 3, 7),
+    (444, 4, 9),
+    (555, 5, 8.5);
 
-INSERT INTO Materias_Profesores (CodigoMateria, NumeroPersonal)
-VALUES
-    (1112, 9284),  -- Materia: Computacion basica, Profesor: Pedro Martínez
-    (2223, 8285),  -- Materia: Matematicas discretas, Profesor: Ana Gómez
-    (3334, 7586),  -- Materia: Sistemas Operativos, Profesor: Carlos Sánchez
-    (4445, 6187),  -- Materia: Experiencia recepcional, Profesor: Guillermo Humberto
-    (5556, 7288);  -- Materia: Eleccion Libre, Profesor: Juana Elisa
+
 
 INSERT INTO Oferta (CodigoMateria, NumeroPersonal, CodigoPeriodo)
 VALUES 
